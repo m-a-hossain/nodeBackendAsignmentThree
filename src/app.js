151f385137abbf775e9f986b1d3ca6cd7d07cdaa
m-a-hossain <adam.eve007@gmail.com>
2021-01-13 19:27:47 +0600

    upto scaffolding

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path =require('path')
 const cors = require('cors')
+const port =3000
+//cors domain server listing maintain
 
 //making application
 const app =express()
@@ -15,16 +17,30 @@ app.use(express.static(path.join(__dirname,'../','public')))
 
 
 
-const port =3000
-//cors domain server listing maintain
+
 
 // for rendering a page
 app.get('/',(req,res)=>{
+   // throw new Error('testing error');
     res.send('<h1>this is simple a page</h1>')
 })
 
 // making custom error handlers
+app.use((req, res, next)=>{
+    const error =new Error('404 not found');
+    error.status = 404;
+    next(error);
+    });
 
+app.use((error, req, res, next)=>{
+    //console.log(error);
+    if(error.status === 404){
+        res.status(404);
+        return res.render('errors/404');
+    }
+  res.status(500);
+  res.render('errors/500');
+});
 
 app.listen(port,()=>{
     console.log(`Example app listening at http://localhost:${port}`)
